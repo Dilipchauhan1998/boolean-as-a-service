a87cdafe17c7f12e12de3156d408d37d16c29a6c
Dilipchauhan1998 <adilipchauhan013@gmail.com>
2020-09-28 10:46:21 +0530

    added iniliazation for tokenAuth repo

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"boolean-as-a-service/auth"
 	"boolean-as-a-service/conn"
 	"boolean-as-a-service/models"
 	"boolean-as-a-service/routes"
@@ -10,8 +11,10 @@ func main() {
 	conn.ConnectDB()
 	db := conn.DB
 	db.AutoMigrate(&models.Boolean{})
+	db.AutoMigrate(&auth.Token{})
 	defer db.Close()
 	models.SetBooleanRepo(conn.DB)
+	auth.SetTokenRepo(conn.DB)
 
 	router := routes.SetupRouter()
 	router.Run(":80")
