878f45218e0c018f97ae0dd64a677a357d52c740
Dilipchauhan1998 <adilipchauhan013@gmail.com>
2020-09-28 10:44:54 +0530

    updated create, get, update, delete boolean handlers to handle token authorization

--- controllers/boolean.go
+++ controllers/boolean.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"boolean-as-a-service/auth"
 	"boolean-as-a-service/models"
 	"net/http"
 	"strings"
@@ -10,9 +11,30 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+//CreateToken ...
+func CreateToken(c *gin.Context) {
+	token := auth.Token{
+		ID: uuid.New().String(),
+	}
+
+	if err := auth.TokenRepo.CreateToken(token); err != nil {
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"token": token.ID,
+	})
+
+}
+
 //CreateBoolean  create a boolean
 func CreateBoolean(c *gin.Context) {
 	var boolean models.Boolean
+
+	if value, _ := c.Request.Header["Token"]; len(value) == 0 || !auth.TokenRepo.ExistToken(value[0]) {
+		c.AbortWithStatus(http.StatusForbidden)
+		return
+	}
 
 	if err := c.BindJSON(&boolean); err != nil {
 		c.AbortWithStatus(http.StatusBadRequest)
@@ -34,6 +56,10 @@ func GetBoolean(c *gin.Context) {
 		boolean models.Boolean
 		err     error
 	)
+	if value, _ := c.Request.Header["Token"]; len(value) == 0 || !auth.TokenRepo.ExistToken(value[0]) {
+		c.AbortWithStatus(http.StatusForbidden)
+		return
+	}
 
 	id := c.Params.ByName("id")
 	if boolean, err = models.BooleanRepo.GetBooleanByID(id); err != nil {
@@ -55,6 +81,11 @@ func UpdateBoolean(c *gin.Context) {
 		boolean models.Boolean
 		err     error
 	)
+
+	if value, _ := c.Request.Header["Token"]; len(value) == 0 || !auth.TokenRepo.ExistToken(value[0]) {
+		c.AbortWithStatus(http.StatusForbidden)
+		return
+	}
 
 	id := c.Params.ByName("id")
 	if err := c.BindJSON(&boolean); err != nil {
@@ -89,8 +120,12 @@ func UpdateBoolean(c *gin.Context) {
 
 //DeleteBoolean ... delete a boolean
 func DeleteBoolean(c *gin.Context) {
-	id := c.Params.ByName("id")
+	if value, _ := c.Request.Header["Token"]; len(value) == 0 || !auth.TokenRepo.ExistToken(value[0]) {
+		c.AbortWithStatus(http.StatusForbidden)
+		return
+	}
 
+	id := c.Params.ByName("id")
 	if _, err := models.BooleanRepo.GetBooleanByID(id); err != nil {
 		if err.Error() == "record not found" {
 			c.AbortWithStatus(http.StatusNotFound)
